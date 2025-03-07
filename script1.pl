7dbffc08e0a07e833a952ae02d5eaa26e4241327
Kartik311995 <kartikasiwal@gmail.com>
2025-03-07 16:55:41 +0530

    Delete script1.pl

--- script1.pl
+++
@@ -1,9 +0,0 @@
-my $name = "Alice";      # Declares a variable $name with local scope
-
-if (1) {
-    my $greeting = "Hello, $name!";
-    print "$greeting\n"; # Can access both $greeting and $name here
-}
-
-print "$name\n";          # Can still access $name here
-# print "$greeting\n";    # Error! $greeting is not accessible here
