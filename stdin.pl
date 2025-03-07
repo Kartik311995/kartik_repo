cf9745874012fefba11ad361a4478ff104c98e02
kartik <kartikasiwal@gmail.com>
2025-03-07 09:03:56 +0000

    stdin

---
+++ stdin.pl
@@ -0,0 +1,11 @@
+#! user/bin/perl
+print "Enter your name: ";
+my $name = <STDIN>;   # Reads a line from user input
+chomp($name);         # Removes the newline at the end
+print "Hello, $name!\n";
+{print "enter two numbers"
+my $num1 = <STDIN>;
+my $num2 =<STDIN>;
+if ($num1 < $num2)
+{print "$num1 is less then $num2";}
+}
